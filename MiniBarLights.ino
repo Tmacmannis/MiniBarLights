87131c466e7be40cf5571149530311bdef4114b0
Tim <timmacmannis@yahoo.com>
2021-03-15 18:01:23 -0400

    code organized

--- MiniBarLights.ino
+++ MiniBarLights.ino
@@ -1,5 +1,6 @@
 #include <FastLED.h>
 #include <credentials.h>
+
 #include "EspMQTTClient.h"
 #include "OTA.h"
 
@@ -17,7 +18,19 @@ CRGB leds2[NUM_LEDS];
 CRGB leds3[NUM_LEDS];
 CRGB leds4[NUM_LEDS];
 
-int brightness = 200;
+boolean lightsOn = true;
+int prevBrightness = 0;
+int brightness = 25;
+unsigned long mqttUpdateTime = 0;
+int currentState;
+
+uint8_t changingHue = 0;
+
+boolean colorTranslated = false;
+
+int red;
+int green;
+int blue;
 
 EspMQTTClient client(
     mySSID,
@@ -61,30 +74,62 @@ void setup() {
 void loop() {
     handleClientTest();
 
-    for(int i=0; i< 20; i++){
-        leds1[i] = CRGB::White;
-        leds2[i] = CRGB::White;
-        leds3[i] = CRGB::White;
-        leds4[i] = CRGB::White;
-    }
-    FastLED.show();
-    delay(1000);
-
-}
-
-void Task1code(void* pvParameters) {
-    TelnetStream.print("Task1 running on core ");
-    TelnetStream.println(xPortGetCoreID());
-
-    for (;;) {
-        delay(28);
-        client.loop();  // takes 60 micro seconds to complete, fast...
+    EVERY_N_MILLISECONDS(16) {
+        ledStateMachine();
+        FastLED.show();
     }
 }
 
-void onConnectionEstablished() {
-    client.subscribe("minibarlights/brightness", [](const String& payload) {
-        TelnetStream.print("payload is: ");
-        TelnetStream.println(payload);
-    });
+void ledStateMachine() {
+    switch (currentState) {
+        case 0:  //solid white
+            for (int i = 0; i < 20; i++) {
+                leds1[i].setRGB(red, green, blue);
+                leds2[i].setRGB(red, green, blue);
+                leds3[i].setRGB(red, green, blue);
+                leds4[i].setRGB(red, green, blue);
+            }
+            break;
+        case 1:  //Slow Change
+            for (int i = 0; i < 20; i++) {
+                leds1[i] = CHSV(changingHue, 255, brightness);
+                leds2[i] = CHSV(changingHue, 255, brightness);
+                leds3[i] = CHSV(changingHue, 255, brightness);
+                leds4[i] = CHSV(changingHue, 255, brightness);
+            }
+            EVERY_N_MILLISECONDS(500){
+                changingHue++;
+            }
+            break;
+        case 2: //Test Pattern
+
+            break;
+            
+    }
 }
+
+
+void setAnimation(String payload) {
+    if (payload == "Solid White") {
+        currentState = 0;
+        red = 255;
+        green = 255;
+        blue = 255;
+    }
+    if (payload == "Slow Change") {
+        currentState = 1;
+    }
+    if (payload == "Scanning") {
+        currentState = 2;
+    }
+    if (payload == "Breathing Multi Color") {
+        currentState = 3;
+    }
+    if (payload == "Scanner") {
+        currentState = 4;
+    }
+    if (payload == "Rainbow") {
+        currentState = 5;
+    }
+}
+
---
+++ mqttFunctions.ino
@@ -0,0 +1,94 @@
+void Task1code(void* pvParameters) {
+    TelnetStream.print("Task1 running on core ");
+    TelnetStream.println(xPortGetCoreID());
+
+    for (;;) {
+        delay(28);
+        client.loop();  // takes 60 micro seconds to complete, fast...
+        unsigned long currentMillis = millis();
+        if (currentMillis - mqttUpdateTime >= 1000) {
+            mqttUpdateTime = currentMillis;
+            //brightness
+            client.publish("minibarlights/brightnessState", String(brightness));
+
+            //On OFf State
+            if (lightsOn) {
+                client.publish("minibarlights/OnOffState", "ON");
+            } else {
+                client.publish("minibarlights/OnOffState", "OFF");
+            }
+        }
+    }
+}
+
+
+void onConnectionEstablished() {
+    client.subscribe("minibarlights/brightness", [](const String& payload) {
+        TelnetStream.print("brightness payload is: ");
+        TelnetStream.println(payload);
+        brightness = map(payload.toInt(), 3, 255, 0, 255);
+        FastLED.setBrightness(brightness);
+    });
+
+    client.subscribe("minibarlights/OnOff", [](const String& payload) {
+        TelnetStream.print("OnOff payload is: ");
+        TelnetStream.println(payload);
+        if (payload == "OFF") {
+            if (lightsOn) {
+                prevBrightness = brightness;
+                brightness = 0;
+                lightsOn = false;
+                FastLED.setBrightness(0);
+            }
+        } else {
+            if (!lightsOn) {
+                brightness = prevBrightness;
+                lightsOn = true;
+                FastLED.setBrightness(brightness);
+            }
+        }
+    });
+
+    client.subscribe("minibarlights/color", [](const String& payload) {
+        TelnetStream.print("Color payload is: ");
+        TelnetStream.println(payload);
+
+        String rval = getValue(payload, ',', 0);
+        String gval = getValue(payload, ',', 1);
+        String bval = getValue(payload, ',', 2);
+
+        red = rval.toInt();
+        green = gval.toInt();
+        blue = bval.toInt();
+    });
+
+    client.subscribe("minibarlights/effects", [](const String& payload) {
+        TelnetStream.print("effects payload is: ");
+        TelnetStream.println(payload);
+        setAnimation(payload);
+    });
+}
+
+String getValue(String data, char separator, int index) {
+    int found = 0;
+    int strIndex[] = {0, -1};
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i + 1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+CHSV hsv2rgb() {
+    CRGB color1temp;
+    color1temp.r = red;
+    color1temp.g = green;
+    color1temp.b = blue;
+    CHSV temp1 = rgb2hsv_approximate(color1temp);
+    return temp1;
+}
